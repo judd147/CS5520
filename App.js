320dd10641e50d6b1fb9ab3e3c6719ad9b015ba6
Liyao Zhang <zhangliyao@Liyaos-MacBook-Pro.local>
2024-01-23 15:00:34 -0800

    finished week3

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Header from './components/Header'
 import Input from './components/Input'
+import React, { useState } from 'react'
 
 export default function App() {
   const appName = 'Demo App';
+  const [text, setText] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function inputHandler(data) {
+    console.log("You typed:", data);
+    setText(data);
+    setModalVisible(false);
+  }
+
+  function showModal() {
+    setModalVisible(true);
+  }
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} version={2}/>
-      <Input />
+      <Button title='Add a Goal' onPress={showModal}/>
+      <Input inputHandler={inputHandler} modalVisible={modalVisible}/>
+      <Text>{text}</Text>
     </View>
     // handleTextChange takes in an onChangeText behind the scenes and we call setText to update the text state
   );
--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,18 @@ import React from 'react'
 export default function Header({ name, version}) {
   return (
     <View>
-      <Text>Welcome to {name}!</Text>
+      <Text style={styles.header}>Welcome to {name}!</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    color: "darkmagenta",
+    fontSize: 20,
+    borderColor: "darkmagenta",
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 5
+  }
+});
